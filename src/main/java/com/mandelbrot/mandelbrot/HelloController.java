3b0d57f99878cf2ca6bf23bb5d28427e28d4a86b
Luuk Bouma <luuk.bouma@student.hu.nl>
2023-04-30 23:36:56 +0200

    it works

--- src/main/java/com/mandelbrot/mandelbrot/HelloController.java
+++ src/main/java/com/mandelbrot/mandelbrot/HelloController.java
@@ -11,59 +11,46 @@ import java.nio.IntBuffer;
 
 public class HelloController {
 
-    @FXML private Canvas canvas;
+
+    @FXML
+    private Canvas canvas;
     PixelWriter pixelWriter;
     GraphicsContext gc;
+    WritableImage writableImage;
 
-    int canvasWidth;
-    int canvasHeight;
+    public int canvasWidth;
+    public int canvasHeight;
     Mandelbrot mandelbrot = new Mandelbrot();
 
-    public int getIntFromColor(int Red, int Green, int Blue){
-        Red = (Red << 16) & 0x00FF0000; //Shift red 16-bits and mask out other stuff
-        Green = (Green << 8) & 0x0000FF00; //Shift Green 8-bits and mask out other stuff
-        Blue = Blue & 0x000000FF; //Mask out anything not blue.
-
-        return 0xFF000000 | Red | Green | Blue; //0xFF000000 for 100% Alpha. Bitwise OR everything together.
-    }
 
 
 
-//    public Boolean calcOnePixel(double x, double y) {
-//        int iteration = 0;
-//        int maxIterations = 1000;
-//        while (x*x + y*y <= 2*2 && iteration < maxIterations) {
-//            int xTemp = x*x - y*y +
-//        }
-//    }
-//
+    //    public Boolean calcOnePixel(double x, double y) {
+    //        int iteration = 0;
+    //        int maxIterations = 1000;
+    //        while (x*x + y*y <= 2*2 && iteration < maxIterations) {
+    //            int xTemp = x*x - y*y +
+    //        }
+    //    }
+    //
     @FXML
     public void updateCanvas() {
-
+        int[] pixels = mandelbrot.makeImage(canvasWidth, canvasHeight, -2.00, 0.47, -1.12, 1.12);
+        pixelWriter.setPixels(0, 0, canvasWidth, canvasHeight, PixelFormat.getIntArgbInstance(), pixels, 0, canvasWidth);
+        gc.drawImage(writableImage, 0, 0);
     }
 
     public void initialize() {
-        gc = canvas.getGraphicsContext2D();
-
         canvasHeight = (int) canvas.getHeight();
         canvasWidth = (int) canvas.getWidth();
-        pixelWriter = canvas.getGraphicsContext2D().getPixelWriter();
-        int[] pixels = new int[canvasWidth * canvasHeight];
-        for (int i = 0; i < pixels.length; i++) {
-            pixels[i] = 0xFF0000FF;
-        }
-        System.out.println(pixels[0]);
-        System.out.println("as");
-        System.out.println(pixels[1]);
-        System.out.println(pixels[2]);
-        System.out.println(pixels[3]);
-        System.out.println(pixels[4]);
-        
 
-        WritableImage writableImage = new WritableImage(canvasWidth, canvasHeight);
+        gc = canvas.getGraphicsContext2D();
+        writableImage = new WritableImage(canvasWidth, canvasHeight);
         pixelWriter = writableImage.getPixelWriter();
-        pixelWriter.setPixels( 0, 0, canvasWidth, canvasHeight, PixelFormat.getIntArgbInstance(), pixels,0, canvasWidth);
-        gc.drawImage(writableImage, 0, 0);
+        //int[] pixels = mandelbrot.makeImage(5, 5, -2.00, 0.47, -1.12, 1.12);
 
+        int[] pixels = mandelbrot.makeImage(canvasWidth, canvasHeight, -2.00, 0.47, -1.12, 1.12);
+        pixelWriter.setPixels(0, 0, canvasWidth, canvasHeight, PixelFormat.getIntArgbInstance(), pixels, 0, canvasWidth);
+        gc.drawImage(writableImage, 0, 0);
     }
 }
--- src/main/java/com/mandelbrot/mandelbrot/Mandelbrot.java
+++ src/main/java/com/mandelbrot/mandelbrot/Mandelbrot.java
@@ -2,11 +2,55 @@ package com.mandelbrot.mandelbrot;
 
 public class Mandelbrot {
 
+    public int getIntFromColor(int red, int green, int blue) {
+        red = (red << 16) & 0x00FF0000; //Shift red 16-bits and mask out other stuff
+        green = (green << 8) & 0x0000FF00; //Shift Green 8-bits and mask out other stuff
+        blue = blue & 0x000000FF; //Mask out anything not blue.
 
-    public void calc() {
+        return 0xFF000000 | red | green | blue; //0xFF000000 for 100% Alpha. Bitwise OR everything together.
+    }
+
+    private int calc(double xPos, double yPos) {
+
+        int maxIterations = 1000;
+        double x = 0;
+        double y = 0;
+        double xTemp = 0;
+
+        for (int iteration = 0; iteration < maxIterations; iteration++) {
+            xTemp = x * x - y * y + xPos;
+            y = 2 * x * y + yPos;
+            x = xTemp;
+        }
+//        while (x*x + y*y <= 2*2 && iteration < maxIterations) {
+//            xTemp = x * x - y * y + xPos;
+//            y = 2 * x * y + yPos;
+//            x = xTemp;
+//            iteration++;
+//        }
+        if ((x*x + y*y) <= 2*2) {
+            return getIntFromColor(45, 45, 70);
+        }
+        return getIntFromColor(25, 255, 255);
 
     }
-    public void makeImage() {
 
+
+    private double pointScaling(double min, double max, int pixelPos, int size) {
+        double step = (max + Math.abs(min)) / size;
+        return min + step * pixelPos;
+    }
+
+    public int[] makeImage(int width, int height, double xMin, double xMax, double yMin, double yMax) {
+        int[] pixels = new int[width * height];
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                double xPoint = pointScaling(xMin, xMax, x, width);
+                double yPoint = pointScaling(yMin, yMax, y, height);
+
+                pixels[width * y + x] = calc(xPoint, yPoint);
+            }
+        }
+        return pixels;
     }
 }
